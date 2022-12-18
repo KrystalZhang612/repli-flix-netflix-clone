36093a0b10805f80f07e8f86e02cba141bee4943
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-17 19:48:50 -0800

    last commit

--- RepliFlix/SceneDelegate.swift
+++ RepliFlix/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions:
         UIScene.ConnectionOptions) {
         
@@ -51,4 +50,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 }
-
