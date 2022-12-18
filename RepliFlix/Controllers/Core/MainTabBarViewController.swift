5ebae7c366b25ac43b93ca5a5e412a9b7d16873d
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-17 19:38:44 -0800

    last commit

--- RepliFlix/Controllers/Core/MainTabBarViewController.swift
+++ RepliFlix/Controllers/Core/MainTabBarViewController.swift
@@ -8,17 +8,14 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
-        
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: UpcomingViewController())
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadsViewController())
-        
-        
+    
         vc1.tabBarItem.image = UIImage(systemName: "house")
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
@@ -31,8 +28,7 @@ class MainTabBarViewController: UITabBarController {
         
         tabBar.tintColor = .label
         
-        setViewControllers([vc1,vc2,vc3,vc4],animated: true)
-        
+        setViewControllers([vc1,vc2,vc3,vc4],animated: true) 
     }
 
 }
